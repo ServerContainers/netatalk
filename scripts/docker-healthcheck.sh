5cf71c79fe4185eba5cbf5784b91e36fbf6c6581
Marvin Winkler <m@rvin.at>
2020-10-16 19:39:43 +0200

    fixed healthcheck

--- scripts/docker-healthcheck.sh
+++ scripts/docker-healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[[ $(ps aux | grep '[n]etatalk -d\|[a]vahi-daemon\|[d]bus-daemon\|[r]unsvdir' | wc -l) -ge '4' ]]
+[[ $(ps aux | grep '[n]etatalk -d\|[a]vahi-daemon\|[r]unsvdir' | wc -l) -ge '3' ]]
 exit $?
