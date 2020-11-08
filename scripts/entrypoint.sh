8d426af9fed413ebfc9445cd25d2a8199fd5337a
Marvin Winkler <m@rvin.at>
2020-11-08 07:42:08 +0100

    added avahi service name

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -96,7 +96,7 @@ if [ ! -f "$INITALIZED" ]; then
 
   done
 
-  [ ! -z ${AVAHI_NAME+x} ] && echo ">> ZEROCONF: custom avahi afp.service name: $AVAHI_NAME" && sed 's/%h/'"$AVAHI_NAME"'/g' /etc/avahi/services/afp.service
+  [ ! -z ${AVAHI_NAME+x} ] && echo ">> ZEROCONF: custom avahi afp.service name: $AVAHI_NAME" && sed -i 's/%h/'"$AVAHI_NAME"'/g' /etc/avahi/services/afp.service
 
   echo ">> ZEROCONF: afp.service file"
   echo "############################### START ####################################"
